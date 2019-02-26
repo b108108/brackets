5ce9a5a355d7665b83ed23ebdc0daeb21cc3a535
b108108 <b108@tut.by>
2019-02-27 02:16:09 +0300

    Task 5 - brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,94 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = []
+  const closeBrackets = []
+  const trigger = []
+  const stack = []
+
+  bracketsConfig.forEach((item) => {
+    openBrackets.push(item[0])
+    closeBrackets.push(item[1])
+    
+    if (item[0] === item[1]) {
+      trigger.push(0)
+    } else {
+      trigger.push(1)
+    }
+  })
+
+  if (str === '') {
+    return true
+  }
+
+  len = str.length
+  let position = 0
+  
+  for (let item = 0; item < len; item++) {
+    
+    if (str[item] !== ' ') {
+      position = openBrackets.indexOf(str[item])
+      
+      if ( position > -1) {
+        
+        // if bracket is equal
+        if (openBrackets[position] === closeBrackets[position]) {
+          
+          if (stack.length > 0) {
+            
+            // check brackets prev type with current type
+            if (stack[stack.length-1] === str[item]) {
+              stack.pop()
+              trigger[position]--;
+              
+              if (trigger[position] < 0) {
+                // error
+                return false;
+              }
+
+            } else {
+              // add equal bracket to stack
+              stack.push(str[item])
+              trigger[position]++;
+            }
+
+          } else {
+            // add first open bracket to stack
+            stack.push(str[item])
+            trigger[position]++;
+          }          
+
+        } else {
+          // add open bracket to stack
+          stack.push(str[item])
+        }
+
+      } else {
+        position = closeBrackets.indexOf(str[item])
+        
+        if ( position > -1) {
+          
+          if (stack.length > 0) {
+
+            // check bracket prev type with current type
+            if (openBrackets.indexOf(stack[stack.length-1]) === position) {
+              // remove open brackets from stack, because current bracket is close
+              stack.pop()
+            } else {
+              // error - incorrect brackets
+              return false;
+            }
+
+          } else {
+            // error - first bracket is close
+            return false;
+          }          
+        }
+      }
+    }
+  }
+
+  if (stack.length > 0) {
+    return false
+  } else {
+    return true
+  }
 }
